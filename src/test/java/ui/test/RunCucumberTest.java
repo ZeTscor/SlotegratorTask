e4ca75e419f76388bb00b5e347cf754f5da7275d
artem <poi.ru0130@gmail.com>
2022-04-03 02:05:30 +0300

    add tag start

--- src/test/java/ui/test/RunCucumberTest.java
+++ src/test/java/ui/test/RunCucumberTest.java
@@ -5,11 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.jupiter.api.Tag;
 import org.junit.runner.RunWith;
 
-@Tag("UI")
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"classpath:features/adminPanel.feature"},
         glue = {"ui.test"})
-@Tag("UI")
 public class RunCucumberTest {
 }
